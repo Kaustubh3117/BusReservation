8805d905db3d0f58797b9693bb9aa356fa088721
unknown <kaustubh3117@gmail.com>
2022-02-04 09:27:14 -0800

    adding djoser

--- BusReservation/urls.py
+++ BusReservation/urls.py
@@ -4,8 +4,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('auth/', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
     # path('auth/', include('djoser.social.urls')),
 ]
 
