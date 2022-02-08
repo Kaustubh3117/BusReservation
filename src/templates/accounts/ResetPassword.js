6b7a940fd47b98ae52c1d4d3add46889ed19b712
unknown <kaustubh3117@gmail.com>
2022-02-07 20:12:16 -0800

    password reset password changes

--- src/templates/accounts/ResetPassword.js
+++ src/templates/accounts/ResetPassword.js
@@ -1,47 +1,118 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate  } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { reset_password } from '../../stores/accounts/actions/AuthActions';
+import { Button } from "primereact/button";
+import { classNames } from "primereact/utils";
+import { Card } from "primereact/card";
+import { InputText } from "primereact/inputtext";
+// react hook form
+import { useForm, Controller } from "react-hook-form";
 
 const ResetPassword = ({ reset_password }) => {
     const [requestSent, setRequestSent] = useState(false);
-    const [formData, setFormData] = useState({
-        email: ''
-    });
-
-    const { email } = formData;
-
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
-
-    const onSubmit = e => {
-        e.preventDefault();
-
-        reset_password(email);
-        setRequestSent(true);
+    const [formData, setFormData] = useState({});
+    const defaultValues = {
+      email: "",
     };
+    const {
+      control,
+      formState: { errors },
+      handleSubmit,
+    } = useForm({ defaultValues });
+  
+    const onSubmit = (e) => {
+      setFormData(e);
+    };
+  
+    useEffect(() => {
+      if (Object.values(formData).length > 0) {
+        reset_password(formData.email);
+        setRequestSent(true);
+      }
+    }, [formData]);
+    const getFormErrorMessage = (name) => {
+        return (
+          errors[name] && <small className="p-error">{errors[name].message}</small>
+        );
+      };
+    // const [formData, setFormData] = useState({
+    //     email: ''
+    // });
+
+    // const { email } = formData;
+
+    // const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    // const onSubmit = e => {
+    //     e.preventDefault();
+
+        // reset_password(email);
+        // setRequestSent(true);
+    // };
 
     if (requestSent) {
         return <Navigate  to='/' />
     }
 
     return (
-        <div className='container mt-5'>
-            <h1>Request Password Reset:</h1>
-            <form onSubmit={e => onSubmit(e)}>
-                <div className='form-group'>
-                    <input
-                        className='form-control'
-                        type='email'
-                        placeholder='Email'
-                        name='email'
-                        value={email}
-                        onChange={e => onChange(e)}
-                        required
-                    />
+        <>
+        <div className="form-account mt-8">
+        <div className="text-center">
+          <div className="grid">
+            <div className="col-4"></div>
+            <div className="col-4">
+              <Card className="shadow-4">
+                <div className="my-5 mx-6">
+                  <h1>Request Password Reset</h1>
+                  <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
+                    <div className="field mt-4">
+                      <span className="p-float-label p-input-icon-right">
+                        <i className="pi pi-envelope" />
+                        <Controller
+                          name="email"
+                          control={control}
+                          rules={{
+                            required: "Email is required.",
+                            pattern: {
+                              value:
+                                /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                              message:
+                                "Invalid email address. E.g. example@email.com",
+                            },
+                          }}
+                          render={({ field, fieldState }) => (
+                            <InputText
+                              id={field.name}
+                              {...field}
+                              name="email"
+                              className={classNames({
+                                "p-invalid": fieldState.invalid,
+                              })}
+                            />
+                          )}
+                        />
+                        <label
+                          htmlFor="email"
+                          className={classNames({ "p-error": !!errors.email })}
+                        >
+                          Email*
+                        </label>
+                      </span>
+                      {getFormErrorMessage("email")}
+                    </div>
+                    <Button type="submit" label="Reset Password" className="mt-2" />
+                  </form>
                 </div>
-                <button className='btn btn-primary' type='submit'>Reset Password</button>
-            </form>
+              </Card>
+            </div>
+            <div className="col-4"></div>
+          </div>
         </div>
+      </div>
+        </>
+       
+        
     );
 };
 
--- src/templates/accounts/ResetPasswordConfirm.js
+++ src/templates/accounts/ResetPasswordConfirm.js
@@ -1,35 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Navigate  } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { reset_password_confirm } from '../../stores/accounts/actions/AuthActions';
+import { useParams } from 'react-router-dom';
+import { Button } from "primereact/button";
+import { classNames } from "primereact/utils";
+import { Card } from "primereact/card";
+import { Password } from "primereact/password";
+import { Divider } from "primereact/divider";
+// react hook form
+import { useForm, Controller } from "react-hook-form";
 
 const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
+    const { uid, token } = useParams();
     const [requestSent, setRequestSent] = useState(false);
-    const [formData, setFormData] = useState({
-        new_password: '',
-        re_new_password: ''
-    });
+    const [formData, setFormData] = useState({});
+    const [passwordMatched, setPasswordMatched] = useState(false);
+  const defaultValues = {
+    new_password: "",
+    re_new_password: "",
+  };
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({ defaultValues });
+    // const [formData, setFormData] = useState({
+    //     new_password: '',
+    //     re_new_password: ''
+    // });
+    const onSubmit = (e) => {
+        setFormData(e);
+      };
+    
+      useEffect(() => {
+        if (Object.values(formData).length > 0 &&
+        formData.new_password === formData.re_new_password) {
+          reset_password_confirm(uid, token, formData.new_password, formData.re_new_password);
+          setRequestSent(true);
+        } else if (
+            Object.values(formData).length > 0 &&
+            formData.new_password !== formData.re_new_password
+          ) {
+            setPasswordMatched(true);
+          }
+      }, [formData]);
 
-    const { new_password, re_new_password } = formData;
+      //old data
+    // const { new_password, re_new_password } = formData;
 
-    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+    // const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
-    const onSubmit = e => {
-        e.preventDefault();
-
-        const uid = match.params.uid;
-        const token = match.params.token;
-
-        reset_password_confirm(uid, token, new_password, re_new_password);
-        setRequestSent(true);
-    };
+    // const onSubmit = e => {
+    //     e.preventDefault();
+    //     reset_password_confirm(uid, token, new_password, re_new_password);
+    //     setRequestSent(true);
+    // };
+    const getFormErrorMessage = (name) => {
+        return (
+          errors[name] && <small className="p-error">{errors[name].message}</small>
+        );
+      };
+    const passwordHeader = <h6>Pick a password</h6>;
+    const passwordFooter = (
+      <React.Fragment>
+        <Divider />
+        <p className="mt-2">Suggestions</p>
+        <ul className="pl-2 ml-2 mt-0" style={{ lineHeight: "1.5" }}>
+          <li>At least one lowercase</li>
+          <li>At least one uppercase</li>
+          <li>At least one numeric</li>
+          <li>Minimum 8 characters</li>
+        </ul>
+      </React.Fragment>
+    );
 
     if (requestSent) {
         return <Navigate  to='/' />
     }
 
     return (
-        <div className='container mt-5'>
+        <>
+       
+        {/* <div className='container mt-5'>
             <form onSubmit={e => onSubmit(e)}>
             <div className='form-group'>
                     <input
@@ -57,7 +110,91 @@ const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
                 </div>
                 <button className='btn btn-primary' type='submit'>Reset Password</button>
             </form>
+        </div> */}
+
+        <div className="form-account mt-8">
+        <div className="text-center">
+          <div className="grid">
+            <div className="col-4"></div>
+            <div className="col-4">
+              <Card className="shadow-4">
+                <div className="my-5 mx-6">
+                  <h1>Reset Password</h1>
+                  <form onSubmit={handleSubmit(onSubmit)} className="p-fluid">
+                  <div className="field mt-5">
+                      <span className="p-float-label">
+                        <Controller
+                          name="new_password"
+                          control={control}
+                          rules={{ required: "Password is required." }}
+                          render={({ field, fieldState }) => (
+                            <Password
+                              id={field.name}
+                              {...field}
+                              toggleMask
+                              name="new_password"
+                              className={classNames({
+                                "p-invalid": fieldState.invalid,
+                              })}
+                              header={passwordHeader}
+                              footer={passwordFooter}
+                            />
+                          )}
+                        />
+                        <label
+                          htmlFor="new_password"
+                          className={classNames({ "p-error": errors.new_password })}
+                        >
+                          Password*
+                        </label>
+                      </span>
+                      {getFormErrorMessage("new_password")}
+                    </div>
+                    <div className="field mt-5">
+                      <span className="p-float-label">
+                        <Controller
+                          name="re_new_password"
+                          control={control}
+                          rules={{ required: "Confirm Password is required." }}
+                          render={({ field, fieldState }) => (
+                            <Password
+                              id={field.name}
+                              {...field}
+                              toggleMask
+                              name="re_new_password"
+                              className={classNames({
+                                "p-invalid": fieldState.invalid,
+                              })}
+                            />
+                          )}
+                        />
+                        <label
+                          htmlFor="re_new_password"
+                          className={classNames({
+                            "p-error": errors.re_new_password,
+                          })}
+                        >
+                          Confirm Password*
+                        </label>
+                      </span>
+                      {getFormErrorMessage("re_new_password")}
+
+                      {passwordMatched ? (
+                        <small className="p-error">
+                          Password did not matched
+                        </small>
+                      ) : null}
+                    </div>
+                    <Button type="submit" label="Confirm Reset Password" className="mt-2" />
+                  </form>
+                </div>
+              </Card>
+            </div>
+            <div className="col-4"></div>
+          </div>
         </div>
+      </div>
+      </>
     );
 };
 
